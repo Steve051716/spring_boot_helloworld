e80cec81c4957ee0b4c1d719ca86f4108403ee1f
steve0517@163.com <steve0517@163.com>
2018-09-10 18:10:19 +0800

    add to git

--- src/main/java/com/gyh/helloworld/HelloWorldController.java
+++ src/main/java/com/gyh/helloworld/HelloWorldController.java
@@ -2,13 +2,14 @@ package com.gyh.helloworld;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
     @RequestMapping("/index")
-    public String index() {
+    public @ResponseBody String index() {
         return "hello";
     }
 }
